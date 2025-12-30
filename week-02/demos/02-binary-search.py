9f58a790ae47484f6a7680e19f6a80d169b5ace1
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-30 11:08:52 +0530

    binary search implementation

--- week-02/demos/02-binary-search.py
+++ week-02/demos/02-binary-search.py
@@ -1,6 +1,18 @@
 def binary_search(list, item):
-    ??
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = int((low + high)/2)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if item > guess:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return None
     
     
 nos = [1,3,5,7,9] # nos 'should' be ordered (sorted for binary search to work)
-print(binary_search(nos, 5) # expected output : 2
+print(binary_search(nos, 5)) # expected output : 2
+print(binary_search(nos, 11)) # expected output : None (indicating non-existent item)
