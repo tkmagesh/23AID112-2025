ab94ba6c36ef71c192c418e758e56ef9471fd0d1
Magesh Kuppan <tkmagesh77@gmail.com>
2026-01-19 09:50:49 +0530

    updates

--- week-05/oop-discussion/01-student.py
+++ week-05/oop-discussion/01-student.py
@@ -9,19 +9,42 @@ class Student:
     """
     def __init__(self):
         print("a new student object is created")
-        self.id = 123
-        self.name = "Magesh"
-        self.location = "Bangalore"
+        self.id = 0
+        self.name = ""
+        self.location = ""
         
     def whoAmI(self):
-        print(self.id, self.name, self.location)
+        print(f"Id={self.id}, Name={self.name}, Location={self.location}")
 
 
-s1 = Student() # creating a new Student object
+# creating a new Student object
+s1 = Student() 
+
+# Assign the values to the attributes
+s1.id = 101
+s1.name = "Magesh"
+s1.location = "Bangalore"
+
+# accessing the attributes (state)
+""" 
+print(s1.id)
+print(s1.name)
+print(s1.location)
+"""
+
+# method invocation
 s1.whoAmI()
 
+
+
 s2 = Student() # creating another new Student object
+s2.id = 102
+s2.name = "Suresh"
+s2.location = "Chennai"
 s2.whoAmI()
 
 s3 = Student() # creating another new Student object
+s3.id = "103"
+s3.name = "Rajesh"
+s3.location = "Villivakkam"
 s3.whoAmI()
