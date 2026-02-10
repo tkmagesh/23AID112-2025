99365a5a5407438f6e1085ac55fe8d4b19a1804f
Magesh Kuppan <tkmagesh77@gmail.com>
2026-02-10 10:48:35 +0530

    merge sort demo

---
+++ week-06/merge-sort.py
@@ -0,0 +1,43 @@
+indent = "  "
+invocation_count=0
+
+def merge_sort(arr):
+    print(" ")
+    global indent, invocation_count
+    indent += "  "
+    invocation_count += 1
+    
+    if len(arr) <= 1:
+        return arr
+    print(f"{indent} [merge_sort({invocation_count})] arr =", arr)
+    mid = len(arr) // 2
+    
+    print(f"{indent} [merge_sort({invocation_count})] arr[:{mid}] = {arr[:mid]}, arr[{mid}:] = {arr[mid:]}")
+    
+    left = merge_sort(arr[:mid])
+    print(f"{indent} [merge_sort({invocation_count})] left =", left)
+    
+    right = merge_sort(arr[mid:])
+    print(f"{indent} [merge_sort({invocation_count})] right =", right)
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    print(f"{indent} [merge({invocation_count})] left={left}, right={right}")
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    print(f"{indent} [merge({invocation_count})] result={result}")
+    return result
+
+arr = [38, 27, 43, 3, 9]
+print(merge_sort(arr))
